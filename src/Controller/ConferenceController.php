3564beb4bdfc5039ab9202f50bf0772f92564bf0
Adrien Roches <adrien@roc-it.tech>
2025-12-10 16:11:07 +0100

    Fill in the ConfereceController to save data

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -5,13 +5,24 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Conference;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
 
 final class ConferenceController
 {
-    #[Route('/conference/{name}/{start}/{end}', name: 'app_conference_new')]
-    public function newConference(string $name, string $start, string $end, /* ... */): Response
+    #[Route(
+        path: '/conference/{name}/{start}/{end}',
+        name: 'app_conference_new',
+        requirements: [
+            'name' => Requirement::ASCII_SLUG,
+            'start' => Requirement::DATE_YMD,
+            'end' => Requirement::DATE_YMD,
+        ],
+        methods: ['GET']
+    )]
+    public function newConference(string $name, string $start, string $end, EntityManagerInterface $em): Response
     {
         $conference = (new Conference())
             ->setName($name)
@@ -21,7 +32,8 @@ final class ConferenceController
             ->setEndAt(new \DateTimeImmutable($end))
         ;
 
-        //...
+        $em->persist($conference);
+        $em->flush();
 
         return new Response('Conference created');
     }
