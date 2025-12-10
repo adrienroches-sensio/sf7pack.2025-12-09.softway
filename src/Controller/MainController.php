d3e8152fbd2c4d479ad3b77cc0a2edf5d375c9f8
Adrien Roches <adrien@roc-it.tech>
2025-12-10 10:32:56 +0100

    Add Request to controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,14 +5,17 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class MainController extends AbstractController
 {
-    #[Route(path: '/', name: 'homepage')]
-    public function index(): Response
+    #[Route(path: '/', name: 'homepage', methods: ['GET'])]
+    public function index(Request $request): Response
     {
-        return new Response('Hello World!');
+        $name = $request->query->get('name', 'World');
+
+        return new Response("Hello {$name}!");
     }
 }
