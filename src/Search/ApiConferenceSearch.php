27e0eac3fde3bf8c21f0ed3174d621a81818202e
Adrien Roches <adrien@roc-it.tech>
2025-12-17 14:18:28 +0100

    Add API Key for Devevents

--- src/Search/ApiConferenceSearch.php
+++ src/Search/ApiConferenceSearch.php
@@ -4,8 +4,16 @@ declare(strict_types=1);
 
 namespace App\Search;
 
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
 final class ApiConferenceSearch implements ConferenceSearchInterface
 {
+    public function __construct(
+        #[Autowire(env: 'CONFERENCES_API_KEY')]
+        private readonly string $apiKey,
+    ) {
+    }
+
     public function searchByName(?string $name = null): array
     {
         // TODO: Implement searchByName() method.
