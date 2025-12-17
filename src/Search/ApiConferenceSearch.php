46c4c73445af6ee05500681618bd04174c86b208
Adrien Roches <adrien@roc-it.tech>
2025-12-17 15:10:39 +0100

    Use Scoped client for Conferences API

--- src/Search/ApiConferenceSearch.php
+++ src/Search/ApiConferenceSearch.php
@@ -5,27 +5,21 @@ declare(strict_types=1);
 namespace App\Search;
 
 use Symfony\Component\DependencyInjection\Attribute\AsAlias;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\DependencyInjection\Attribute\Target;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[AsAlias(id: ConferenceSearchInterface::class)]
 final class ApiConferenceSearch implements ConferenceSearchInterface
 {
     public function __construct(
+        #[Target('conferences.client')]
         private readonly HttpClientInterface $httpClient,
-
-        #[Autowire(env: 'CONFERENCES_API_KEY')]
-        private readonly string $apiKey,
     ) {
     }
 
     public function searchByName(?string $name = null): array
     {
         $options = [
-            'headers' => [
-                'apikey' => $this->apiKey,
-                'accept' => 'application/json',
-            ],
             'query' => []
         ];
 
@@ -35,7 +29,7 @@ final class ApiConferenceSearch implements ConferenceSearchInterface
             $options['query']['name'] = $name;
         }
 
-        $response = $this->httpClient->request('GET', 'https://devevents-api.fr/events', $options);
+        $response = $this->httpClient->request('GET', '/events', $options);
         dump($response->toArray());
 
         return [];
